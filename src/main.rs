86fa11a39d8e8f75597197cbe3e5e849b1c4ece8
Lodny <drinkjuice@naver.com>
2023-08-27 21:48:42 +0900

    1. create ctx.rs

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,12 @@ use serde::Deserialize;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
 
+use crate::ctx::Ctx;
 use crate::model::ModelController;
 
 pub use self::error::{Error, Result};
 
+mod ctx;
 mod error;
 mod model;
 mod web;
@@ -40,6 +42,9 @@ async fn main() -> Result<()> {
         .serve(route_all.into_make_service())
         .await
         .expect("Failed to start server");
+
+    let ctx = Ctx::new(10);
+    ctx.user_id()
 
     Ok(())
 }
