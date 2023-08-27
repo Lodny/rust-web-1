dc9c4488ab34ad178dabba9eb79dd7801c01eda1
Lodny <drinkjuice@naver.com>
2023-08-27 21:13:55 +0900

    1. add mw_auth.rs

---
+++ src/web/mw_auth.rs
@@ -0,0 +1,23 @@
+use axum::http::Request;
+use axum::middleware::Next;
+use axum::response::Response;
+use tower_cookies::Cookies;
+
+use crate::{Error, Result};
+use crate::web::AUTH_TOKEN;
+
+pub async fn mw_require_auth<B>(
+    cookies: Cookies,
+    req: Request<B>,
+    next: Next<B>,
+) -> Result<Response> {
+    println!("->> {:<12} - mw_require_auth", "MIDDLEWARE");
+
+    let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
+    // println!(">>> mw_require_auth : {:?}", auth_token);
+
+    // TODO: Real auth-token parsing & validation.
+    auth_token.ok_or(Error::AuthFailNoAuthTokenCookie)?;
+
+    Ok(next.run(req).await)
+}
