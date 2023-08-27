6c7327fd9c3831445095b2d9d49eb0578b90c594
Lodny <drinkjuice@naver.com>
2023-08-27 16:17:40 +0900

    3. create model controller

--- src/model.rs
+++ src/model.rs
@@ -1,20 +1,67 @@
 //! Simplistic Model Layer
 //! (with mock-store layer)
 
+use std::sync::{Arc, Mutex};
+
 use serde::{Deserialize, Serialize};
+
+use crate::{Error, Result};
 
 // region:      -- Ticket Types
 #[derive(Clone, Debug, Serialize)]
 pub struct Ticket {
-    id: u64,
-    title: String,
+    pub id: u64,
+    pub title: String,
 }
 
 #[derive(Deserialize)]
 pub struct TicketForCreate {
-    title: String,
+    pub title: String,
 }
-
-
 // endregion:   -- Ticket Types
 
+
+// region:      -- Model Controller
+#[derive(Clone)]
+pub struct ModelController {
+    ticket_store: Arc<Mutex<Vec<Option<Ticket>>>>,
+}
+
+impl ModelController {
+    // Constructor
+    pub async fn new() -> Result<Self> {
+        Ok(Self {
+            ticket_store: Arc::default(),
+        })
+    }
+
+    pub async fn create_ticker(&self, ticket_fc: TicketForCreate) -> Result<Ticket> {
+        let mut store = self.ticket_store.lock().unwrap();
+
+        let id = store.len() as u64;
+        let ticket = Ticket {
+            id,
+            title: ticket_fc.title,
+        };
+        store.push(Some(ticket.clone()));
+
+        Ok(ticket)
+    }
+
+    pub async fn list_ticker(&self) -> Result<Vec<Ticket>> {
+        let store = self.ticket_store.lock().unwrap();
+
+        let tickets = store.iter().filter_map(|t| t.clone()).collect();
+
+        Ok(tickets)
+    }
+
+    pub async fn delete_ticker(&self, id: u64) -> Result<Ticket> {
+        let mut store = self.ticket_store.lock().unwrap();
+
+        let ticket = store.get_mut(id as usize).and_then(|t| t.take());
+
+        ticket.ok_or(Error::TicketDeleteFailIdNotFount { id })
+    }
+}
+// region:      -- Model Controller
